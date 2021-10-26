3a2d4dccccc2dc051f9440fce314a0f6b1f9c7dc
DiegoDevol <diegoASsousa@gmail.com>
2021-10-26 17:05:40 -0300

    add origin user create boteria

--- src/app/controllers/user/CreateUserController.js
+++ src/app/controllers/user/CreateUserController.js
@@ -7,153 +7,185 @@ import userSchema from '../../../models/user';
 import logger from '../../../utils/logger';
 
 class CreateUserController {
-    async create(request, response) {
-        try {
+  async create(request, response) {
+    try {
+      const { user } = request.body;
 
-            const { user } = request.body;
+      const salt = bcrypt.genSaltSync(10);
 
-            const salt = bcrypt.genSaltSync(10);
+      const hashPassword = user.password;
 
-            const hashPassword = user.password;
+      const hash = bcrypt.hashSync(hashPassword, salt);
 
-            const hash = bcrypt.hashSync(hashPassword, salt);
+      let createUserBoteria;
 
-            let createUserBoteria;
+      if (user.resource === 'partner') {
+        const source = user.origin === 'nuvemshop' ? 'app_nuvemshop' : 'app_rd';
 
-            if (user.resource === 'partner') {
+        const userBoteria = {
+          name: user.name,
+          email: user.email,
+          phone: user.phone,
+          companyName: user.companyName,
+          password: user.password,
+          origin: 'partners',
+          source,
+          registerFrom: {
+            type: 'PARTNERS',
+            _id: process.env.REGISTER_FROM_ID,
+          },
+        };
 
-                const userBoteria = {
-                    name: user.name,
-                    email: user.email,
-                    phone: user.phone,
-                    companyName: user.companyName,
-                    password: user.password,
-                    source: 'LP_nuvemshop',
-                    registerFrom: {
-                        type: 'PARTNERS',
-                        _id: process.env.REGISTER_FROM_ID
-                    }
-                };
+        createUserBoteria = await boteriaService(
+          userBoteria,
+          user.tokenReCaptcha
+        );
+      }
 
-                createUserBoteria = await boteriaService(userBoteria, user.tokenReCaptcha);
-            }
+      if (
+        user.dashboardToken !== undefined ||
+        createUserBoteria.status === 200
+      ) {
+        const userEmail = await userSchema.find({ email: user.email });
 
-            if (user.dashboardToken !== undefined || createUserBoteria.status === 200) {
+        if (userEmail.length === 0) {
+          const objCopyTemplate = {
+            companyName: user.companyName,
+            origin: user.origin === undefined ? 'boteria' : user.origin,
+            companyId:
+              createUserBoteria !== undefined
+                ? createUserBoteria.companyId
+                : user.companyId,
+            organizationId:
+              createUserBoteria !== undefined
+                ? createUserBoteria.organizationId
+                : user.organizationId,
+            userId:
+              createUserBoteria !== undefined
+                ? createUserBoteria.userId
+                : user.userId,
+          };
 
-                const userEmail = await userSchema.find({ "email": user.email });
+          let copyTemplate = '';
 
-                if (userEmail.length === 0) {
+          if (user.resource === 'partner') {
+            copyTemplate = await copyTemplateBotService(
+              objCopyTemplate,
+              user.code
+            );
+          }
 
-                    const objCopyTemplate = {
-                        companyName: user.companyName,
-                        origin: user.origin === undefined ? 'boteria' : user.origin,
-                        companyId: createUserBoteria !== undefined ? createUserBoteria.companyId : user.companyId,
-                        organizationId: createUserBoteria !== undefined ? createUserBoteria.organizationId : user.organizationId,
-                        userId: createUserBoteria !== undefined ? createUserBoteria.userId : user.userId
-                    }
+          if (copyTemplate !== '' && user.resource !== 'partner') {
+            logger.error('Erro inesperado, tente novamente');
 
-                    let copyTemplate = '';
+            return response.status(200).send({
+              result: 'error',
+              message: 'Erro inesperado, tente novamente',
+              user: userEmail,
+              status: 422,
+            });
+          }
 
-                    if (user.resource === 'partner') {
-                        copyTemplate = await copyTemplateBotService(objCopyTemplate, user.code);
-                    }
+          let botPublished;
 
-                    if (copyTemplate !== '' && user.resource !== 'partner') {
+          if (
+            copyTemplate === '' ||
+            copyTemplate._id === null ||
+            copyTemplate._id === 'null' ||
+            copyTemplate._id === '' ||
+            copyTemplate === undefined
+          ) {
+            botPublished = 1;
+          } else {
+            botPublished = copyTemplate._id;
+          }
 
-                        logger.error('Erro inesperado, tente novamente');
+          if (user.origin === 'nuvemshop') {
+            await nuvemshopService(
+              user.userIdStore,
+              user.accessToken,
+              botPublished
+            );
+          }
 
-                        return response.status(200).send({
-                            'result': 'error',
-                            'message': 'Erro inesperado, tente novamente',
-                            'user': userEmail,
-                            'status': 422
-                        });
-                    } else {
+          const integrationArray = [];
 
-                        let botPublished;
+          if (user.resource === 'partner') {
+            const integration = {
+              name: user.origin,
+            };
 
-                        if (copyTemplate === '' || copyTemplate._id === null || copyTemplate._id === 'null' || copyTemplate._id === '' || copyTemplate === undefined) {
-                            botPublished = 1
-                        } else {
-                            botPublished = copyTemplate._id;
-                        }
+            botPublished !== 1
+              ? (integration.templateBotId = botPublished)
+              : '';
 
-                        if (user.origin === 'nuvemshop') {
-                            await nuvemshopService(user.userIdStore, user.accessToken, botPublished);
-                        }
+            user.origin !== undefined
+              ? (integration.accessToken = user.accessToken)
+              : '';
+            user.origin === 'rd'
+              ? (integration.refreshToken_rd = user.refreshToken)
+              : (integration.userIdStore = user.userIdStore);
+            user.origin === 'rd' ? (integration.code = user.code) : '';
+            user.origin === 'rd' ? (integration.redirectValue = 1) : '';
 
-                        const integrationArray = [];
+            const integrationBoteria = {
+              name: 'boteria',
+              userIdBoteria: createUserBoteria.userId,
+              dashboardToken: createUserBoteria.dashboardToken,
+              companyId: createUserBoteria.companyId,
+              organizationId: createUserBoteria.organizationId,
+            };
 
-                        if (user.resource === 'partner') {
-                            const integration = {
-                                name: user.origin,
-                            }
+            integrationArray.push(integration, integrationBoteria);
+          } else {
+            const integrationBoteria = {
+              name: 'boteria',
+              userIdBoteria: user.userId,
+              dashboardToken: user.dashboardToken,
+              companyId: user.companyId,
+              organizationId: user.organizationId,
+            };
 
-                            botPublished !== 1 ? integration.templateBotId = botPublished : '';
+            integrationArray.push(integrationBoteria);
+          }
 
-                            user.origin !== undefined ? integration.accessToken = user.accessToken : ''
-                            user.origin === 'rd' ? integration.refreshToken_rd = user.refreshToken : integration.userIdStore = user.userIdStore
-                            user.origin === 'rd' ? integration.code = user.code : ''
-                            user.origin === 'rd' ? integration.redirectValue = 1 : '';
+          const userSave = {
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            companyName: user.companyName,
+            password: hash,
+            botPublish: botPublished,
+            originInitial: user.origin === undefined ? 'boteria' : user.origin,
+            integrations: integrationArray,
+          };
 
-                            const integrationBoteria = {
-                                name: 'boteria',
-                                userIdBoteria: createUserBoteria.userId,
-                                dashboardToken: createUserBoteria.dashboardToken,
-                                companyId: createUserBoteria.companyId,
-                                organizationId: createUserBoteria.organizationId,
-                            }
+          await createUserService(userSave);
 
-                            integrationArray.push(integration, integrationBoteria);
-                        } else {
-                            const integrationBoteria = {
-                                name: 'boteria',
-                                userIdBoteria: user.userId,
-                                dashboardToken: user.dashboardToken,
-                                companyId: user.companyId,
-                                organizationId: user.organizationId
-                            }
-
-                            integrationArray.push(integrationBoteria);
-                        }
-
-                        const userSave = {
-                            name: user.name,
-                            email: user.email,
-                            phone: user.phone,
-                            companyName: user.companyName,
-                            password: hash,
-                            botPublish: botPublished,
-                            originInitial: user.origin === undefined ? 'boteria' : user.origin,
-                            integrations: integrationArray
-                        };
-
-                        await createUserService(userSave);
-
-                        return response.status(200).send({
-                            'result': 'success',
-                            'message': 'Usuário cadastrado com sucesso',
-                            'user': userSave,
-                            'status': 200
-                        });
-                    }
-                }
-
-                logger.error('Usuário já existente');
-
-                return response.status(200).send({
-                    'result': 'error',
-                    'message': 'Usuário já existente, insira outro email',
-                    'user': userEmail,
-                    'status': 422
-                });
-            } else if (createUserBoteria.status === 422) {
-                return response.status(200).send(createUserBoteria);
-            }
-        } catch (error) {
-            return response.json(error);
+          return response.status(200).send({
+            result: 'success',
+            message: 'Usuário cadastrado com sucesso',
+            user: userSave,
+            status: 200,
+          });
         }
+
+        logger.error('Usuário já existente');
+
+        return response.status(200).send({
+          result: 'error',
+          message: 'Usuário já existente, insira outro email',
+          user: userEmail,
+          status: 422,
+        });
+      }
+      if (createUserBoteria.status === 422) {
+        return response.status(200).send(createUserBoteria);
+      }
+    } catch (error) {
+      return response.json(error);
     }
+  }
 }
 
 export default new CreateUserController();
